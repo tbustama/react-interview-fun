3287e5284fb0f39b7e100bf5bd1c608a0edcf82a
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-04-23 10:10:22 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+const { useEffect, useState } = React;
+
+function getRandomUsers(pageNumber) {
+  return fetch(`https://randomuser.me/api?page=${pageNumber}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then(({ results }) => {
+      console.log(results);
+      return results;
+    })
+    .catch((err) => console.log(err));
+}
+
+const getUserInfo = (userInfo) => {
+  const name = userInfo.name;
+  const fullName = `${name.title} ${name.first} ${name.last}`;
+  return fullName;
+};
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [randomUserDataJSON, setRandomUserData] = useState([]);
+  const [nextPage, setNextPage] = useState(1);
+
+  const fetchNextUser = () => {
+    getRandomUsers(nextPage).then((randomData) => {
+      if (randomData == undefined) return;
+      const newUserInfos = [...randomUserDataJSON, ...randomData];
+      setRandomUserData(newUserInfos || ["Nothing here to see"]);
+      setNextPage(nextPage + 1);
+    });
+  };
+
+  useEffect(() => {
+    fetchNextUser();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{counter}</p>
+        <button onClick={() => setCounter(counter + 1)}>
+          Increase Count
+        </button>{" "}
+        <br></br>
+        <button onClick={fetchNextUser}>Get More Users</button>
+        <br></br>
+        {randomUserDataJSON.map((userInfo, index) => (
+          <>
+            <p>{getUserInfo(userInfo)}</p>
+            <img src={userInfo.picture.thumbnail} />
+          </>
+        ))}
+        {/* <p> {randomUserDataJSON}</p> */}
       </header>
     </div>
   );
